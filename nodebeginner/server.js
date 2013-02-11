8afb3bef4b0129c1d14e3926dc56fe77759b2823
Mathias Dalheimer <md@gonium.net>
2013-02-11 15:15:46 +0100

    implemented a nice POST handler.

--- nodebeginner/server.js
+++ nodebeginner/server.js
@@ -4,9 +4,19 @@ var port = 8888;
 
 function start(router, handle) {
   function onRequest(request, response) {
+    var postdata = "";
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
-    router(handle, pathname, response);
+
+    request.setEncoding("utf8");
+
+    request.addListener("data", function(postDataChunk) {
+      postdata += postDataChunk;
+      console.log("received POST data chunk '"+postDataChunk+"'.");
+    });
+    request.addListener("end", function() {
+      router(handle, pathname, response, postdata);
+    });
   }
   http.createServer(onRequest).listen(port);
   console.log("Server listening on port", port);

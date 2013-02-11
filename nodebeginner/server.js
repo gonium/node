e18622319e2899cacca0a96c9f331e03ea2b1ec8
Mathias Dalheimer <md@gonium.net>
2013-02-11 15:48:30 +0100

    finished tutorial - image upload now working.

--- nodebeginner/server.js
+++ nodebeginner/server.js
@@ -4,19 +4,9 @@ var port = 8888;
 
 function start(router, handle) {
   function onRequest(request, response) {
-    var postdata = "";
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
-
-    request.setEncoding("utf8");
-
-    request.addListener("data", function(postDataChunk) {
-      postdata += postDataChunk;
-      console.log("received POST data chunk '"+postDataChunk+"'.");
-    });
-    request.addListener("end", function() {
-      router(handle, pathname, response, postdata);
-    });
+    router(handle, pathname, response, request);
   }
   http.createServer(onRequest).listen(port);
   console.log("Server listening on port", port);

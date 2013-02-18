335421576e55f8e27481c75d0e560f256029c756
Mathias Dalheimer <md@gonium.net>
2013-02-18 14:30:13 +0100

    added EJS frontend based on bootstrap

--- socket.io/app.js
+++ socket.io/app.js
@@ -7,11 +7,6 @@ var app = module.exports = express();
 var server=require("http").createServer(app)
   , io = require('socket.io').listen(server)
   , redis = require('redis')
-var rclient=redis.createClient();
-
-rclient.on("error", function(err) {
-  console.log("Redis client error: " + err);
-});
 
 
 // see http://stackoverflow.com/questions/4600952/node-js-ejs-example
@@ -19,15 +14,19 @@ rclient.on("error", function(err) {
 
 app.use(express.errorHandler());
 app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
-  res.render('index.jade', {title: "Horst"});
+  res.render('index.ejs', {title: "Horst"});
 });
 app.use(express.static(__dirname + '/public'));
 
 server.listen(PORT);
 
 io.sockets.on('connection', function (socket) {
+  var rclient=redis.createClient();
+  rclient.on("error", function(err) {
+    console.log("Redis client error: " + err);
+  });
   rclient.subscribe(channel_current_measurements);
   rclient.on("message", function(channel, msg) {
     console.log(channel + ": " +msg);
